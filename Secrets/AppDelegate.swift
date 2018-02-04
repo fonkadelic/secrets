f44e3e6d98aa225c4596895f36fc8aa603bce917
Bernhard Loibl <mail@bernhardloibl.com>
2018-02-04 13:24:37 +0100

    Initial commit.

---
+++ Secrets/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  Secrets
+//
+//  Created by Bernhard Loibl on 27.01.18.
+//  Copyright © 2018 Bernhard Loibl. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+
+        return true
+    }
+}
+
